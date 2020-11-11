26ba2c7bbd3e3b53f8d71c6ea7359f196c6715ba
Dang Anh Tu nguyen <danganhtunguyen@dangs-mbp.lan>
2020-11-11 11:58:46 -0800

    revert commit to austin

--- application/views/home.py
+++ application/views/home.py
@@ -1,7 +1,10 @@
 # THIS IS HOME BLUEPRINT for home, about, login and signup
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request,redirect,url_for,session
 from db import SearchingDB
+
 db = SearchingDB()
+
+
 
 # create a blue print
 home = Blueprint('home', __name__)
@@ -10,21 +13,52 @@ home = Blueprint('home', __name__)
 def homepage():
     postings = db.getAllPostings()
     lst = db.getPostingOrganizedData(postings)
+    if 'loggedin' in session:
+        return render_template('home/home.html', data = lst, user=session['firstname'])
     return render_template('home/home.html', data = lst)
+    
 @home.route('/about')
 def about():
+    if 'loggedin' in session:
+        return render_template("home/about.html",user=session['firstname'])
     return render_template("home/about.html")
 
 @home.route('/about/<name>')
 def getPerson(name):
     item = f"home/about/{name}.html"
+    if 'loggedin' in session:
+        return render_template(item, name = name,user=session['firstname'])
     return render_template(item, name = name)
 
-@home.route('/login')
+@home.route('/login/',methods =['GET','POST'])
 def login():
-    return render_template("home/login.html")
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'pwd' in request.form:
+        email=request.form['email']
+        password=request.form['pwd']
+        user=db.getAUser("email",email)
+        if user:
+            if email==user[0] and password==user[1]:
+                session['loggedin']=True
+                session['email']=user[0]
+                session['firstname']=user[3]
+                return redirect(url_for("home.homepage"))
+        else:
+            msg='Invalid Email/Password'
+    return render_template("home/login.html", msg=msg)
+
 
 @home.route('/signup')
 def signup(name):
     return render_template("home/signup.html")
+
+@home.route('/logout')
+def logout():
+    session.pop('loggedin',None)
+    session.pop('email',None)
+    session.pop('firstname',None)
+    return redirect(url_for("home.homepage"))
+
+
+
 
