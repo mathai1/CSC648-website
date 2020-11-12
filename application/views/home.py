209bd5c00620caf9745e3225b2236d026a7994dc
Dang Anh Tu nguyen <danganhtunguyen@dangs-mbp.lan>
2020-11-12 03:40:11 -0800

    fixing some login

--- application/views/home.py
+++ application/views/home.py
@@ -1,5 +1,5 @@
 # THIS IS HOME BLUEPRINT for home, about, login and signup
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect, url_for
 from db import SearchingDB
 
 db = SearchingDB()
@@ -30,22 +30,23 @@ def getPerson(name):
         return render_template(item, name = name,user=session['firstname'])
     return render_template(item, name = name)
 
-@home.route('/login/',methods =['GET','POST'])
+@home.route('/login',methods =['GET','POST'])
 def login():
-    msg = ''
-    if request.method == 'POST' and 'email' in request.form and 'pwd' in request.form:
-        email=request.form['email']
-        password=request.form['pwd']
-        user=db.getAUser("email",email)
-        if user:
-            if email==user[0] and password==user[1]:
-                session['loggedin']=True
-                session['email']=user[0]
-                session['firstname']=user[3]
-                return redirect(url_for("home.homepage"))
-        else:
-            msg='Invalid Email/Password'
-    return render_template("home/login.html", msg=msg)
+    if request.method == 'POST':
+        email =request.form['email']
+        password =request.form['password']
+        account = db.checkAUser(email,password)
+        if account :
+            # session['loggedin'] = True
+            session['email'] = request.form['email']
+            user = db.getUserOrganizedData(account)
+            session['name'] = user[0]['fname']
+            postings = db.getAllPostings()
+            postinglst = db.getPostingOrganizedData(postings)
+            return render_template("home/home.html", data = postinglst, user = user)
+        else :
+            return render_template("home/login.html", message = "email or password is incorrect")
+    return render_template("home/login.html")
 
 
 @home.route('/signup', methods=['GET', 'POST'])
@@ -60,16 +61,12 @@ def signup():
         return render_template("home/login.html" , message = "Account is created")
     return render_template("home/signup.html")
 
-<<<<<<< HEAD
 @home.route('/logout')
 def logout():
-    session.pop('loggedin',None)
     session.pop('email',None)
-    session.pop('firstname',None)
+    session.pop('name',None)
     return redirect(url_for("home.homepage"))
 
 
 
-=======
->>>>>>> dangbranch
 
