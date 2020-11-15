fed03aa743ddd7cff5175205ac3f6d402a8905ed
Dang Anh Tu nguyen <danganhtunguyen@dangs-mbp.lan>
2020-11-14 23:43:33 -0800

    fix price filter

--- application/views/search.py
+++ application/views/search.py
@@ -8,31 +8,44 @@ search = Blueprint('search', __name__)
 @search.route('/search', methods= ['GET' , 'POST'])
 def searchpage():
     if request.method == 'POST':
-        print(request.form)
-        category =request.form['filter']
-        searchedData =request.form['searchedData']
-        postings = db.searchAPosting(category,searchedData)
-        lst = db.getPostingOrganizedData(postings)
-        # if 'loggedin' in session:
-        #     return render_template("search/search.html", data = lst,user=session['firstname'])
-        return render_template("search/search.html", data = lst, searchedData = searchedData, category = category)
-    return render_template("search/search.html")
-
-@search.route('/filter', methods= ['GET' , 'POST'])
-def filterpage():
-    if request.method == 'POST':
-        print(request.args)
-        category =request.args['searchedData']
-        searchedData =request.args['category']
-        if 'min' in request.args and 'max' in request.args :
+        print("FORM",request.form)
+        print("ARGS:",request.args)
+        # thisguy = []
+        searchedData = ""
+        category = ""
+        postings = []
+        if 'min' in request.args and 'max' in request.args:
+            # thisguy = request.args
             min = request.args['min']
             max = request.args['max']
-            postings = db.getPostingbyPrice(min, max, category,searchedData)
-            print(postings)
-            return render_template("search/search.html", data = postings, searchedData = searchedData, category = category)
-        # lst = db.getPostingOrganizedData(postings)
-        # if 'loggedin' in session:
-        #     return render_template("search/search.html", data = lst,user=session['firstname'])
-        # return render_template("search/search.html", data = lst, searchedData = searchedData, category = category)
+            category = request.args['filter']
+            searchedData =request.args['searchedData']
+            postings = db.getPostingbyPrice(min, max, searchedData, category)
+            return render_template("search/search.html", data = postings, searchedData = searchedData, category = category, min = min , max = max)       
+        else:
+            # thisguy = request.form
+            category = request.form['filter']
+            searchedData = request.form['searchedData']
+            postings = db.searchAPosting(category,searchedData)     
+            lst = db.getPostingOrganizedData(postings)
+            return render_template("search/search.html", data = lst, searchedData = searchedData, category = category)
     return render_template("search/search.html")
+
+# @search.route('/filter', methods= ['GET' , 'POST'])
+# def filterpage():
+#     if request.method == 'POST':
+#         print(request.args)
+#         category =request.args['searchedData']
+#         searchedData =request.args['category']
+#         if 'min' in request.args and 'max' in request.args :
+#             min = request.args['min']
+#             max = request.args['max']
+#             postings = db.getPostingbyPrice(min, max, category,searchedData)
+#             print(postings)
+#             return render_template("search/search.html", data = postings, searchedData = searchedData, category = category)
+#         # lst = db.getPostingOrganizedData(postings)
+#         # if 'loggedin' in session:
+#         #     return render_template("search/search.html", data = lst,user=session['firstname'])
+#         # return render_template("search/search.html", data = lst, searchedData = searchedData, category = category)
+#     return render_template("search/search.html")
 
